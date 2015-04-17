16e065e7866da71cf8c091593866b6acd7509eee
Will Ross <paxswill@gmail.com>
2015-04-17 17:21:00 -0400

    Make EFT headers link to Osmium (and maybe work in-game)
    Also add a vim control line to the CSS file.

---
+++ script.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+  var $headers = jQuery('.eft-header');
+  // Convert EFT headers to Osmium links
+  $headers.each(function(index, element) {
+    var $element = jQuery(element),
+        dna = $element.data('dna'),
+        url = "https://o.smium.org/loadout/dna/" + dna + '?mangle=0',
+        $newHeader = jQuery("<a href='#'></a>");
+    if(typeof CCPEVE !== "undefined") {
+      // It doesn't seem like the IGB actually executes this JS file. If it
+      // does, this /should/ work, but it's a pretty narrow use case.
+      $newHeader.on('click', function(ev) {
+        CCPEVE.showFitting(dna);
+        return false;
+      });
+    } else {
+      $newHeader.attr('href', url);
+    }
+    $newHeader.text($element.text());
+    $element.replaceWith($newHeader);
+  });
+});
+// vim:ts=2:sw=2:et:
