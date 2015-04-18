1b701f1e05d607fc5cabd0f327c633762b30c5ac
Will Ross <paxswill@gmail.com>
2015-04-18 13:19:42 -0400

    Add labels to the configuration settings

---
+++ lang/en/settings.php
@@ -0,0 +1,5 @@
+<?php
+$lang['dsn'] = 'DSN connection string for a database containing the EVE SDE.';
+$lang['username'] = 'The username for the database (optional).';
+$lang['password'] = 'The password for the database (optional).';
+
